7f16f2367b4468219fa7f3fa4d54f1dff3931322
vsevolodgrigorev <sevagrigoryev@gmail.com>
2024-04-18 19:23:14 +0300

    Изменения под мак

--- src/test/java/gui/testing/Testing.java
+++ src/test/java/gui/testing/Testing.java
@@ -3,12 +3,14 @@ package gui.testing;
 import gui.testing.commo.AppTestCase;
 import gui.testing.commo.GlobalVars;
 import gui.testing.commo.ScreenshotComparer;
+
 import org.assertj.swing.finder.JOptionPaneFinder;
 import org.assertj.swing.fixture.JOptionPaneFixture;
 import org.assertj.swing.timing.Pause;
 import org.junit.Test;
 
-import java.awt.*;
+import java.awt.Component;
+import java.awt.AWTException;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -26,21 +28,34 @@ public class Testing extends AppTestCase {
         JOptionPaneFixture inputDialog = JOptionPaneFinder.findOptionPane()
                 .withTimeout(GlobalVars.TIMEOUT).using(robot());
 
-        inputDialog.textBox().enterText("10");
+        inputDialog.textBox().enterText("1");
         inputDialog.buttonWithText("OK").click();
 
         Pause.pause(5000);
 
-        // Taking a screenshot of the relevant component
-        // Replace 'graphComponent' with the actual component that displays the graph
-        Component graphComponent = window.panel("chartPanel").target();
+        // Делаем скриншот соответствующего компонента
+        // Замените 'graphComponent' на реальный компонент, который отображает график
+         Component graphComponent = window.panel("chartPanel").target();
+        // Для Mac
+        File screenshot = takeScreenshot(graphComponent,
+                "sinGraphTest", "/Users/vsevolodgrigorev/Desktop/диссер/workApp/result");
+
+        // Comparing the screenshot with a reference image
+        String etalonPath = "/Users/vsevolodgrigorev/Desktop/диссер/workApp/etalon/";
+        String resultPath = "/Users/vsevolodgrigorev/Desktop/диссер/workApp/diff/";
+/*
+//  Для PC
         File screenshot = takeScreenshot(graphComponent,
                 "sinGraphTest", "C:\\Users\\vislavdis\\IdeaProjects\\test");
 
         // Comparing the screenshot with a reference image
         String etalonPath = "C:\\Users\\vislavdis\\IdeaProjects\\test\\etalon\\";
         String resultPath = "C:\\Users\\vislavdis\\IdeaProjects\\test\\result\\";
-        int delta = 0; // Adjust delta as needed for acceptable color variance
+*/
+
+        // Отрегулировать дельту так, как это необходимо для получения приемлемой цветовой дисперсии
+        // Желательно чтобы она была равно 0
+        int delta = 0;
 
         ScreenshotComparer comparer = new ScreenshotComparer() {
             @Override
